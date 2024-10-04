92141c6bcfd65861eea569f3b738d34f1da5502e
Zach Rawlings <Zachrg42@gmail.com>
2024-10-03 20:11:57 -0600

    Created Animator and its Script

---
+++ actuallytruefinalone/Assets/Scripts/CharacterAnimationController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimationController : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       HandleAnimations(); 
+    }
+    
+    private void HandleAnimations()
+        {
+            if (Input.GetAxis("Horizontal") !=0)
+            {
+                animator.SetTrigger("Run");
+            }
+            else
+            {
+                animator.SetTrigger("Idle");
+            }
+            
+            if (Input.GetButtonDown("Jump"))
+            {
+                animator.SetTrigger("Jump");
+            }
+            
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                animator.SetTrigger("WallJump");
+            }   
+        }
+}
