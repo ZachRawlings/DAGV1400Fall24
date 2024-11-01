9b9e3a97e42fa855a6d0d1d27878405214e7894f
Zach Rawlings <Zachrg42@gmail.com>
2024-11-01 16:33:37 -0600

    Got a working Healthbar
    Healthbar Now decreases when taking damage from the saw

---
+++ actuallytruefinalone/Assets/Scripts/SimpleImageBehavior.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class SimpleImageBehaviour : MonoBehaviour
+{
+    public SimpleFloatData dataObj;
+    private Image _imageObj;
+    
+
+    private void Start()
+    {
+        _imageObj = GetComponent<Image>();
+    }
+
+    public void UpdateWithFloatData()
+    {
+        _imageObj.fillAmount = dataObj.value;
+    }
+}
